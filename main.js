733b775011158481b0b4f405e5c4e6147dea7d84
msostaric <54621273+msostaric-hub@users.noreply.github.com>
2020-09-10 17:41:54 +0200

    Korekcija

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ let quotes = [
     '“First, solve the problem. Then, write the code.” – John Johnson',
     '“Java is to JavaScript what car is to Carpet.” – Chris Heilmann',
     '“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler',
-    '“ Code is like humor. When you have to explain it, it’s bad.” – Cory House',
-    '“Optimism is an occupational hazard of programming: feedback is the treatment. “ Kent Beck',
+    '“Code is like humor. When you have to explain it, it’s bad.” – Cory House',
+    '“Optimism is an occupational hazard of programming: feedback is the treatment.“ Kent Beck',
     '“Simplicity is the soul of efficiency.” – Austin Freeman',
     '“Before software can be reusable it first has to be usable.” – Ralph Johnson',
     '“It’s harder to read code than to write it.” — Joel Spolsky',
-    '"Deleted code is debugged code". - Jeff Sickel'
+    '"Deleted code is debugged code." - Jeff Sickel'
 ];
 
 btn.addEventListener('click', function(){
