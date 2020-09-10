0beda85bde759fce3b12bd137b7b2f845387196e
msostaric <54621273+msostaric-hub@users.noreply.github.com>
2020-09-10 17:07:34 +0200

    Finalna verzija

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 let quotes = [
-    '"You only live once, but if you do it right, once is enough." - Mae West',
-    '"I am so clever that sometimes I don\'t understand a single word of what I am saying." - Oscar Wilde',
-    '"Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.", Albert Einstein',
-    '"The most beautiful experience we can have is the mysterious. It is the fundamental emotion that stands at the cradle of true art and true science.", Albert Einstein'
+    '“First, solve the problem. Then, write the code.” – John Johnson',
+    '“Java is to JavaScript what car is to Carpet.” – Chris Heilmann',
+    '“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler',
+    '“ Code is like humor. When you have to explain it, it’s bad.” – Cory House',
+    '“Optimism is an occupational hazard of programming: feedback is the treatment. “ Kent Beck',
+    '“Simplicity is the soul of efficiency.” – Austin Freeman',
+    '“Before software can be reusable it first has to be usable.” – Ralph Johnson',
+    '“It’s harder to read code than to write it.” — Joel Spolsky',
+    '"Deleted code is debugged code". - Jeff Sickel'
 ];
 
 btn.addEventListener('click', function(){
