5ed4250c08e8aadc33e5d35dec166032d01612f2
msostaric <54621273+msostaric-hub@users.noreply.github.com>
2020-09-10 23:59:27 +0200

    Korekcija
    Dodana crtica kod citata

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let quotes = [
     '“Java is to JavaScript what car is to Carpet.” – Chris Heilmann',
     '“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler',
     '“Code is like humor. When you have to explain it, it’s bad.” – Cory House',
-    '“Optimism is an occupational hazard of programming: feedback is the treatment.“ Kent Beck',
+    '“Optimism is an occupational hazard of programming: feedback is the treatment.“ - Kent Beck',
     '“Simplicity is the soul of efficiency.” – Austin Freeman',
     '“Before software can be reusable it first has to be usable.” – Ralph Johnson',
     '“It’s harder to read code than to write it.” — Joel Spolsky',
